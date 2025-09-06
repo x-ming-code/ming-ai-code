7ca3a3df9ff698130b6bd8727ea83699dcfd4db2
徐明杰 <11270119+quietly-grasp-tang@user.noreply.gitee.com>
2025-09-06 17:51:50 +0800

    用户模块后端 1. 完成基本功能、人工测试通过

--- src/main/java/com/ming/mingaicode/service/AppService.java
+++ src/main/java/com/ming/mingaicode/service/AppService.java
@@ -1,7 +1,12 @@
 package com.ming.mingaicode.service;
 
+import com.ming.mingaicode.model.dto.app.AppQueryRequest;
+import com.ming.mingaicode.model.dto.app.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.ming.mingaicode.model.entity.App;
+
+import java.util.List;
 
 /**
  *  服务层。
@@ -9,5 +14,11 @@ import com.ming.mingaicode.model.entity.App;
  * @author <a href="https://ming-code.work/">ming</a>
  */
 public interface AppService extends IService<App> {
+    // 关联查询用户信息
+    AppVO getAppVO(App app);
 
+    //构造查询对象
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    List<AppVO> getAppVOList(List<App> appList);
 }
--- src/main/java/com/ming/mingaicode/service/impl/AppServiceImpl.java
+++ src/main/java/com/ming/mingaicode/service/impl/AppServiceImpl.java
@@ -1,17 +1,113 @@
 package com.ming.mingaicode.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
+import com.ming.mingaicode.exceptioon.BusinessException;
+import com.ming.mingaicode.exceptioon.ErrorCode;
+import com.ming.mingaicode.model.dto.app.AppQueryRequest;
+import com.ming.mingaicode.model.dto.app.AppVO;
+import com.ming.mingaicode.model.entity.User;
+import com.ming.mingaicode.model.vo.UserVO;
+import com.ming.mingaicode.service.UserService;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.spring.service.impl.ServiceImpl;
 import com.ming.mingaicode.model.entity.App;
 import com.ming.mingaicode.mapper.AppMapper;
 import com.ming.mingaicode.service.AppService;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
- *  服务层实现。
+ * 服务层实现。
  *
  * @author <a href="https://ming-code.work/">ming</a>
  */
 @Service
-public class AppServiceImpl extends ServiceImpl<AppMapper, App>  implements AppService{
+public class AppServiceImpl extends ServiceImpl<AppMapper, App> implements AppService {
+
+    @Resource
+    private UserService userService;
+
+    // 关联查询用户信息
+    @Override
+    public AppVO getAppVO(App app) {
+        if (app == null) {
+            return null;
+        }
+        AppVO appVO = new AppVO();
+        BeanUtil.copyProperties(app, appVO);
+        // 关联查询用户信息
+        Long userId = app.getUserId();
+        if (userId != null) {
+            User user = userService.getById(userId);
+            UserVO userVO = userService.getUserVO(user);
+            appVO.setUser(userVO);
+        }
+        return appVO;
+    }
+
+    //构造查询对象
+    @Override
+    public QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest) {
+        if (appQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "请求参数为空");
+        }
+        Long id = appQueryRequest.getId();
+        String appName = appQueryRequest.getAppName();
+        String cover = appQueryRequest.getCover();
+        String initPrompt = appQueryRequest.getInitPrompt();
+        String codeGenType = appQueryRequest.getCodeGenType();
+        String deployKey = appQueryRequest.getDeployKey();
+        Integer priority = appQueryRequest.getPriority();
+        Long userId = appQueryRequest.getUserId();
+        String sortField = appQueryRequest.getSortField();
+        String sortOrder = appQueryRequest.getSortOrder();
+        return QueryWrapper.create()
+                .eq("id", id)
+                .like("appName", appName)
+                .like("cover", cover)
+                .like("initPrompt", initPrompt)
+                .eq("codeGenType", codeGenType)
+                .eq("deployKey", deployKey)
+                .eq("priority", priority)
+                .eq("userId", userId)
+                .orderBy(sortField, "ascend".equals(sortOrder));
+    }
+
+    //分页查询用户创建的应用信息
+    @Override
+    public List<AppVO> getAppVOList(List<App> appList) {
+        if (CollUtil.isEmpty(appList)) {
+            return new ArrayList<>();
+        }
+        // 批量获取用户信息，避免 N+1 查询问题
+        Set<Long> userIds = appList.stream()
+                .map(app -> {
+                    return app.getUserId();
+                }).collect(Collectors.toSet());
+        List<User> users = userService.listByIds(userIds);
+        //将用户id作为键 UserVO 对象为值的 Map
+        Map<Long, UserVO> userVOMap = users.stream()
+                .collect(Collectors.toMap(
+                        user -> user.getId(),
+                        user -> userService.getUserVO(user)
+                ));
+//        Map<Long, UserVO> userVOMap = userService.listByIds(userIds).stream()
+//                .collect(Collectors.toMap(User::getId, userService::getUserVO));
+        //将一个 App 对象列表，转换为带有用户信息的 AppVO 对象列表。
+        return appList.stream().map(app -> {
+            AppVO appVO = getAppVO(app);
+            UserVO userVO = userVOMap.get(app.getUserId());
+            appVO.setUser(userVO);
+            return appVO;
+        }).collect(Collectors.toList());
+    }
+
 
 }
