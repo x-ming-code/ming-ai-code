f2bdaeb3b2e90430fd76b62d7fd52353b51a4b98
徐明杰 <11270119+quietly-grasp-tang@user.noreply.gitee.com>
2025-09-13 16:20:58 +0800

    后端-工程项目生成（推理模型生成 Vue 工程项目） 1. 新增 Vue 工程推理模型配置 2. 新增 Vue 工程 AI 生成方法 3. 根据生成模式选择 AI 服务（对应修改缓存）

---
+++ src/main/java/com/ming/mingaicode/config/ReasoningStreamingChatModelConfig.java
@@ -0,0 +1,50 @@
+package com.ming.mingaicode.config;
+
+import dev.langchain4j.model.chat.StreamingChatModel;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ming
+ * @description 推理流式模型配置类
+ * @date 2025/9/12 16:16
+ */
+@Configuration
+@Slf4j
+@ConfigurationProperties(prefix = "langchain4j.open-ai.streaming-chat-model")
+@Data
+public class ReasoningStreamingChatModelConfig {
+
+
+    private String baseUrl;
+
+    private String apiKey;
+
+    /**
+     * 推理流式模型（用于 Vue 项目生成，带工具调用）
+     */
+
+    @Bean
+    public StreamingChatModel reasoningStreamingChatModel() {
+        // 为了测试方便临时修改
+        final String modelName = "qwen3-coder-plus";
+        final int maxTokens = 8192;
+        // 生产环境使用：
+        // final String modelName = "deepseek-reasoner";
+        // final int maxTokens = 32768;
+        return OpenAiStreamingChatModel.builder()
+                .apiKey(apiKey)
+                .baseUrl(baseUrl)
+                .modelName(modelName)
+                .maxTokens(maxTokens)
+                .logRequests(true)
+                .logResponses(true)
+                .build();
+
+    }
+
+}
--- src/test/java/com/ming/mingaicode/core/AiCodeGeneratorFacadeTest.java
+++ src/test/java/com/ming/mingaicode/core/AiCodeGeneratorFacadeTest.java
@@ -7,13 +7,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
 
-import java.io.File;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class AiCodeGeneratorFacadeTest {
+    @Resource
+    private AiCodeGeneratorFacade aiCodeGeneratorFacade;
 
+    @Test
+    void generateVueProjectCodeStream() {
+        Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream(
+                "简单的任务记录网站，总代码量不超过 200 行",
+                CodeGenTypeEnum.VUE_PROJECT, 1L);
+        // 阻塞等待所有数据收集完成
+        List<String> result = codeStream.collectList().block();
+        // 验证结果
+        Assertions.assertNotNull(result);
+        String completeContent = String.join("", result);
+        Assertions.assertNotNull(completeContent);
+    }
 
 }
